a1d441ed362d2ed2cff49506be514583925a3f05
manucastelnovo <marnutskate@gmail.com>
2023-01-27 18:04:29 -0300

    creacion de entidades

--- app.py
+++ app.py
@@ -4,6 +4,18 @@ from flask_sqlalchemy import SQLAlchemy
 app= Flask (__name__)
 db=SQLAlchemy(app)
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(100))
+    email = db.Column(db.String(80))
+    password = db.Column(db.String(20))
+
+class Todo(db.Model):
+    id= db.Column(db.Integer, primary_key = True)
+    description = db.Column(db.String (180))
+    is_completed = db.Column(db.Boolean)
+    user_id =db.Column(db.Integer, db.ForeingKey('user.id'))
+
 @app.route('/')
 def home():
     return 'Hello world'
