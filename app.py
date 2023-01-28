baedfc959c3abf74bfe469bc09b85c3114b75caf
doggbmx <manuelorticosta@gmail.com>
2023-01-28 00:27:19 -0300

    adds lil register form

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for, redirect
+from flask_wtf import FlaskForm
+from wtforms import StringField, EmailField, PasswordField, SubmitField
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SECRET_KEY'] = 'chupetess'
 db = SQLAlchemy(app)
 
-
+class RegisterForm(FlaskForm):
+    username = StringField()
+    email= EmailField()
+    password = PasswordField()
+    submit = SubmitField('Register')
 
 
 class User(db.Model):
@@ -19,20 +26,29 @@ class User(db.Model):
     
 
 
-# class Todo(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     description = db.Column(db.String(180))
-#     is_completed = db.Column(db.Boolean)
-#     user_id = db.Column(db.Integer, db.ForeingKey('user.id'))
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(180))
+    is_completed = db.Column(db.Boolean)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     
-#     def __repr__(self):
-#         return '<todo %r>' % self.todo
+    def __repr__(self):
+        return '<todo %r>' % self.todo
 
 
-@app.route('/', methods=['GET'])
-def home():
-    user = User.query.all()
-    return 'iuesaasheasaeaeassaeehjaseaesaeassa'
+@app.route('/', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+
+    if form.validate_on_submit():
+        new_user = User(name=form.username.data, email=form.email.data, password=form.password.data)
+        db.session.add(new_user)
+        # aca
+        db.session.commit()
+        print('comitee la puta madre')
+        # return redirect(url_for('login'))
+        return redirect(url_for('register'))
+    return render_template('index.html',form=form)
 
 
 if __name__ == '__main__':
