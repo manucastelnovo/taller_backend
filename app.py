bfdca5f2e68b5f2a3426d620cc73790f3dcc45b6
doggbmx <manuelorticosta@gmail.com>
2023-01-27 23:40:21 -0300

    initial commit with db included ;)

--- app.py
+++ app.py
@@ -1,24 +1,37 @@
-from flask import Flask
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 
-app= Flask (__name__)
-db=SQLAlchemy(app)
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+# with app.app_context():
+#     db.create_all()
+
+
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
+    id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50))
     email = db.Column(db.String(80))
     password = db.Column(db.String(20))
+    
+    def __repr__(self):
+        return '<User %r>' % self.username
+    
 
-class Todo(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    description = db.Column(db.String(180))
-    is_completed = db.Column(db.Boolean)
-    user_id = db.Column(db.Integer, db.ForeingKey('user.id'))
 
-@app.route('/')
+# class Todo(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     description = db.Column(db.String(180))
+#     is_completed = db.Column(db.Boolean)
+#     user_id = db.Column(db.Integer, db.ForeingKey('user.id'))
+
+
+@app.route('/', methods=['GET'])
 def home():
-    return 'Hello world'
+    user = User.query.all()
+    return 'iuesaasheasaeaeassaeehjaseaesaeassa'
+
 
 if __name__ == '__main__':
     app.run(debug=True)
