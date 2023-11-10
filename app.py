07957eb2b3148efdd6b383a414437f5afd0bb39b
manucastelnovo <marnutskate@gmail.com>
2023-11-10 15:46:23 -0300

    x

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
-from flask import Flask, render_template, request, url_for, redirect
+from flask import Flask, render_template, request, url_for, redirect,send_file
 from flask_wtf import FlaskForm
 from wtforms import StringField, EmailField, PasswordField, SubmitField
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import LoginManager, UserMixin, login_required, login_user, current_user, logout_user
 from flask_sqlalchemy import SQLAlchemy
+import qrcode
+from io import BytesIO
+import base64
+import os
+import zipfile
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
@@ -60,7 +65,7 @@ def register():
     form = RegisterForm()
 
     if form.validate_on_submit():
-        hashed_password = generate_password_hash(form.password.data, method='sha256')
+        hashed_password = generate_password_hash(form.password.data)
         new_user = User(name=form.username.data, email=form.email.data, password=hashed_password)
         db.session.add(new_user)
         # aca
@@ -126,6 +131,51 @@ def logout():
     logout_user()
     return redirect(url_for('register'))
     
+
+@app.route('/generate_qr', methods=['POST','GET'])
+def generate_qr():
+    data = request.form.get('data')
+    num_qr = int(request.form.get('num_qr', 1))  # Número de QRs, por defecto 1
+
+    zip_filename = f"qr_codes.zip"
+    with zipfile.ZipFile(zip_filename, 'w') as zip_file:
+        for i in range(1, num_qr + 1):
+            # Crear el código QR
+            qr = qrcode.QRCode(
+                version=1,
+                error_correction=qrcode.constants.ERROR_CORRECT_L,
+                box_size=10,
+                border=4,
+            )
+            qr.add_data(f"{data}_{i}")
+            qr.make(fit=True)
+
+            # Crear una imagen PIL desde el código QR
+            img = qr.make_image(fill_color="black", back_color="white")
+
+            # Guardar la imagen en un archivo temporal
+            img_bytes = BytesIO()
+            img.save(img_bytes)
+            img_bytes.seek(0)
+
+            # Crear un archivo temporal para cada QR
+            temp_filename = f"temp_qr_{i}.png"
+            with open(temp_filename, 'wb') as temp_file:
+                temp_file.write(img_bytes.read())
+
+            # Agregar el archivo al zip
+            zip_file.write(temp_filename, os.path.basename(temp_filename))
+
+            # Eliminar el archivo temporal
+            os.remove(temp_filename)
+
+    # Enviar el archivo zip como una descarga
+    return send_file(zip_filename, as_attachment=True, download_name=zip_filename)
+
+
+@app.route('/input_qr')
+def index():
+    return render_template('input_qr.html')
 
 
 if __name__ == '__main__':
